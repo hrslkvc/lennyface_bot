d5feb8a402303366810e69a3b08ce69595c782e7
hareluuu <hareluuu@gmail.com>
2015-06-04 13:07:30 +0200

    Initial Commit
    Just adding the bot file

---
+++ lennyface_bot.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import time
+import praw
+from login import *
+
+r = praw.Reddit(user_agent ="Test by /u/ccviper")
+r.login(USERNAME, PASSWORD)
+
+subreddit = r.get_subreddit("funny")
+
+replied = []
+
+def replybot():
+   
+    comments = subreddit.get_comments(limit=1000)
+    phrase = "( ͡° ͜ʖ ͡°)"
+    
+    for comment in comments:        
+        comment_body = comment.body.lower()        
+        if phrase in comment_body and comment.id not in replied:
+            comment.reply("( ͡~ ͜ʖ ͡°)")            
+            replied.append(comment.id)
+            
+        else:
+            continue
+
+ while True:
+    replybot()
+    time.sleep(5)
