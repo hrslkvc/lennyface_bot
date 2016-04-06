9822420a9ab58b30f69f12304586c92bdb71c0e9
hareluuu <hareluuu@gmail.com>
2016-04-06 06:26:01 +0200

    updated

--- lennyface_bot.py
+++ lennyface_bot.py
@@ -3,28 +3,29 @@ import time
 import praw
 from login import *
 
-r = praw.Reddit(user_agent ="lennyface reply bot by u/ccviper")
+r = praw.Reddit(user_agent="lennyface reply bot by u/ccviper")
 r.login(USERNAME, PASSWORD)
 
 subreddit = r.get_subreddit("funny+leagueoflegends+pics+gifs")
 
 
 def replybot():
-    replied = []
-    comments = subreddit.get_comments(limit=1000)
-    phrase = "( ͡° ͜ʖ ͡°)"
-    
-    for comment in comments:        
-        comment_body = comment.body.lower()     
-	 
-        if phrase in comment_body and comment.id not in replied:
-            comment.reply("( ͡~ ͜ʖ ͡°)")   
-	    print("Commenting")         
-            replied.append(comment.id)
-            
-        else:
-            continue
+	replied = []
+	comments = subreddit.get_comments(limit=1000)
+	phrase = "( ͡° ͜ʖ ͡°)"
 
- while True:
-    replybot()
-    time.sleep(5)
+	for comment in comments:
+		comment_body = comment.body.lower()
+
+		if phrase in comment_body and comment.id not in replied:
+			comment.reply("( ͡~ ͜ʖ ͡°)")
+			print("Commenting")
+			replied.append(comment.id)
+
+		else:
+			continue
+
+
+while True:
+	replybot()
+	time.sleep(5)
